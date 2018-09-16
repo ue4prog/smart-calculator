70f1ddadaf6226f339d7354aa77c3e2d6837a192
ue4prog <vladtet2000@mail.ru>
2018-09-16 10:56:37 +0300

    not so perfect

--- src/index.js
+++ src/index.js
@@ -1,27 +1,71 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
+
+constructor(initialValue) {
+  this.operands = ['^', '*','/', '-', '+']
+  this.toCount = [initialValue]
   }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
-
-  multiply(number) {
-    // your implementation
-  }
-
-  devide(number) {
-    // your implementation
-  }
-
-  pow(number) {
-    // your implementation
-  }
+add(number) {
+  this.toCount.push('+')
+  this.toCount.push(number)
+  return this
 }
 
+subtract(number) {
+  this.toCount.push('-')
+  this.toCount.push(number)
+  return this
+
+}
+
+multiply(number) {
+  this.toCount.push('*')
+  this.toCount.push(number)
+  return this
+}
+
+devide(number) {
+  this.toCount.push('/')
+  this.toCount.push(number)
+  return this
+}
+
+pow(number) {
+  this.toCount.push('^')
+  this.toCount.push(number)
+  return this
+}
+
+valueOf() {
+  this.operands.forEach((operand) => {
+  for (let j = 0; j <= this.toCount.length; j++) {
+    if (this.toCount[j] === operand) {
+      switch (operand) {
+        case '^': this.toCount[j] = Math.pow(this.toCount[j - 1], this.toCount[j + 1])
+        break
+        case '*': this.toCount[j] = this.toCount[j - 1] * this.toCount[j +1]
+        break
+        case '/': this.toCount[j] =  this.toCount[j - 1] / this.toCount[j +1]
+        break
+        case '+': this.toCount[j] =  this.toCount[j - 1] + this.toCount[j +1]
+        break
+        case '-': this.toCount[j] =  this.toCount[j - 1] - this.toCount[j +1]
+        break
+        default: return 'wow'
+      }
+      this.toCount.splice(j + 1, 1)
+      this.toCount.splice(j - 1, 1)
+      j = 0
+
+    }
+
+  }
+ 
+})
+
+    return  this.toCount[0]
+
+  }
+
+}
 module.exports = SmartCalculator;
